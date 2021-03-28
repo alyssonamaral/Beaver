fa2f3faf969da10de380e4dfbf572f027acdd5d8
Alysson Amaral <alysson.amaral@mobrj.com.br>
2021-03-27 21:48:38 -0400

    login screen

--- Beaver/Beaver/ViewModels/LoginPageViewModel.cs
+++ Beaver/Beaver/ViewModels/LoginPageViewModel.cs
@@ -1,19 +1,52 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Beaver.ViewModels
 {
     public class LoginPageViewModel : ViewModelBase
     {
-        public LoginPageViewModel(INavigationService navigationService)
-            : base(navigationService)
+        private string _user;
+        public string User
+        {
+            get { return _user; }
+            set => SetProperty(ref _user, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set => SetProperty(ref _password, value);
+        }
+
+        DelegateCommand _navigateCommand;
+        public DelegateCommand NavigateCommand => _navigateCommand
+            ?? (_navigateCommand = new DelegateCommand(async () => await ExecuteNavigateCommand()));
+
+        public LoginPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
         {
             Title = "LoginPage";
         }
+        private async Task ExecuteNavigateCommand()
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(User);
+            bool isPasswordEmpty = string.IsNullOrEmpty(Password);
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                await _dialogService.DisplayAlertAsync("Falha ao entrar", "Email e senha precisam ser preenchidos", "OK");
+            }
+            else
+            {
+                //await NavigationService.NavigateAsync(nameof());
+            }
+        }
+
     }
 }
--- Beaver/Beaver/ViewModels/ViewModelBase.cs
+++ Beaver/Beaver/ViewModels/ViewModelBase.cs
@@ -1,26 +1,37 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Beaver.ViewModels
 {
     public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible
     {
-        protected INavigationService NavigationService { get; private set; }
-
         private string _title;
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
+        private ImageSource _logo;
+        public ImageSource Logo
+        {
+            get { return _logo; }
+            set => SetProperty(ref _logo, value);
+        }
 
-        public ViewModelBase(INavigationService navigationService)
+        protected INavigationService NavigationService { get; private set; }
+
+        protected readonly IPageDialogService _dialogService;
+        public ViewModelBase(INavigationService navigationService, IPageDialogService dialogService)
         {
             NavigationService = navigationService;
+            _dialogService = dialogService;
+            Logo = ImageSource.FromResource("Beaver.Assets.Images.BeaverLogo_mini.png");
         }
 
         public virtual void Initialize(INavigationParameters parameters)
